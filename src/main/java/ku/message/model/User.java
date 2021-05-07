d0b0c7bd1b9b13e5d3333c2ac849a4099ed137a8
plumest <plumest.chayathon@gmail.com>
2021-05-07 17:33:10 +0700

    Input Validation

--- src/main/java/ku/message/model/User.java
+++ src/main/java/ku/message/model/User.java
@@ -20,6 +20,7 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
+    private String email;
     private String role;
     private Instant createdAt;
 }
